82dfebd474314a8de3fadae69c0b553d123275f4
Luis Alejandro Vera Moreno <alejandrovemo@gmail.com>
2025-04-06 12:26:49 -0500

    feature: created basic project of svelte

---
+++ sensor-analysis/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="text-2xl font-bold">Welcome to SvelteKit</h1>
+<p class="mt-4">Visit <a href="https://svelte.dev/docs/kit" class="text-blue-500" target="_blank" rel="noopener noreferrer">svelte.dev/docs/kit</a> to read the documentation</p>
